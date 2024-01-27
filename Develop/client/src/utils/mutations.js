c40b19ae323a24336458c6ffafe6a3624cf4f08d
mintyry <gitmintyry@gmail.com>
2024-01-27 14:36:08 -0800

    wrote out all mutations in mutations client file

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,81 @@
+import { gql } from '@apollo/client';
+
+// mutation for creating a user
+export const CREATE_USER = gql`
+mutation createUser($username: String!, $email: String!, $password: String!) {
+  createUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+}
+`;
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+}`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: SaveBookInput!) {
+  saveBook(input: $input) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}`;
+
+export const DELETE_BOOK = gql`
+mutation deleteBook($bookId: ID) {
+  deleteBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}`;
